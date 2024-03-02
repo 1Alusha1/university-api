232a15763f945df61a1a93b23c5d66b9c38bf274
1alusha1 <alusha9636@gmail.com>
2024-03-02 19:45:46 +0200

    separated some functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,6 @@
+import IPlan from "../controllers/plan/plan.interface";
 import { TupdateRecord } from "../controllers/workLoad/types";
+import planModel from "../models/plan.model";
 
 export function createUpdateObject(data: TupdateRecord[]) {
   let obj: any = {};
@@ -8,4 +10,95 @@ export function createUpdateObject(data: TupdateRecord[]) {
     obj[name] = value;
   });
   return obj;
+}
+
+export async function getSemestrRecords(opt: any, planName: string) {
+  let result = [];
+  let first = "";
+  let condition: any = {};
+
+  for (let i = 1; i <= 8; i++) {
+    first = opt[i];
+    condition[first] = { $ne: "" };
+    let obj = await planModel.find({
+      ...condition,
+      planName,
+    });
+
+    result.push({
+      semestr: i,
+      obj,
+    });
+    condition = {};
+  }
+
+  return result;
+}
+
+export function getCredits(semestr: number, data: any, opt: any) {
+  let credits: any;
+  let condition = semestr + 1 >= 8 ? 8 : semestr + 1;
+
+  if (data[opt[semestr]] && data[opt[condition]]) {
+    credits = Number(data[opt[semestr]]) + Number(data[opt[condition]]);
+  } else {
+    credits = data[opt[semestr]];
+  }
+  return { countCredits: Number(credits) };
+}
+
+export function getCourseCredits(semestr: number, data: any, opt: any) {
+  return { countCredits: Number(data[opt[semestr]]) };
+}
+
+export function getCoefficient(semestr: number, data: any, opt: any) {
+  if (data[opt[`c${semestr}`]] === "") {
+    data[opt[`c${semestr}`]] = 1;
+  }
+  if (data[opt[`c${semestr}`]] === "**") {
+    data[opt[`c${semestr}`]] = 1;
+  }
+  if (data[opt[`c${semestr}`]] === "-") {
+    data[opt[`c${semestr}`]] = 1;
+  }
+  return { coefficient: Number(data[opt[`c${semestr}`]]) };
+}
+
+export function getWeek(semestr: number, data: any, opt: any) {
+  return {
+    week:
+      typeof opt[opt[semestr]] === "string"
+        ? opt[opt[semestr]]
+        : opt[opt[semestr]],
+  };
+}
+
+export function getControlForm(semestr: number, data: IPlan) {
+  if (
+    semestr === 1 ||
+    (semestr >= 3 &&
+      semestr <= 8 &&
+      data.nameEducationalComponent === "Фахова іноземна мова")
+  ) {
+    return {
+      controlForm: {
+        exam: "",
+        credit: "З",
+      },
+    };
+  } else {
+    return data.exams
+      ? {
+          controlForm: {
+            exam: "Е",
+            credit: "",
+          },
+        }
+      : {
+          controlForm: {
+            exam: "",
+            credit: "З",
+          },
+        };
+  }
 }
