fc5c1508b9e7dcaca996016ff0eb34c5203e40e2
1alusha1 <alusha9636@gmail.com>
2024-03-02 20:21:54 +0200

    added middlewares to root file and some options to option file

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import connect from "./db";
 import workLoad from "./routes/wokrLoad.route";
 import plan from "./routes/plan.route";
@@ -6,7 +6,9 @@ import subject from "./routes/subject.route";
 import planAnnex from "./routes/planAnnex.route";
 import workPlan from "./routes/workPlan.route";
 import planName from "./routes/planName.route";
+import file from "./routes/file.route";
 
+import fileUpload from "express-fileupload";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
@@ -25,15 +27,21 @@ const options: cors.CorsOptions = {
   origin: "http://localhost:3001",
   preflightContinue: false,
 };
-
+app.use(
+  fileUpload({
+    limits: { fileSize: 500 * 1024 * 1024 },
+  })
+);
 app.use(express.json());
 app.use(cors(options));
+
 app.use("/api/workload", workLoad);
 app.use("/api/workPlan", workPlan);
 app.use("/api/plan", plan);
 app.use("/api/planAnnex", planAnnex);
 app.use("/api/subject", subject);
 app.use("/api/planName", planName);
+app.use("/api/file", file);
 
 connect(process.env.DB_URI as string);
 app.listen(process.env.PORT, () => console.log("server was start"));
